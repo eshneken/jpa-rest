271d00b8decac3c69a10fe49095dc9d7da8e237e
Ed Shnekendorf <ed.shnekendorf@oracle.com>
2017-06-14 10:28:00 -0400

    updated data file and gitignore for idea files

--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -1,2 +1,2 @@
-insert into EMPLOYEES (EMP_ID, FULLNAME, EMAIL) values (007,'Abhishek Gupta','abhi@test.com')
+insert into EMPLOYEES (EMP_ID, FULLNAME, EMAIL) values (007,'Ed Shnekendorf','eshneken@test.com')
 insert into EMPLOYEES (EMP_ID, FULLNAME, EMAIL) values (42,'John Doe','jdoes@test.com')
